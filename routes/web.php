de87367c95dcf7db01bd4efdbac153181faea457
elijah <1198331919@qq.com>
2019-06-20 09:55:06 +0800

    feat(auth): a better redirection after authentication

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,9 @@
 |
 */
 
+// use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 /*Route::get('/', function () {
     return view('welcome');
