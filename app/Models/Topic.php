610eaaed3b33ea9d178c940ae55834ef1b581112
elijah <1198331919@qq.com>
2019-09-10 11:46:08 +0800

    feat(topic): display topic list

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -21,4 +21,15 @@ class Topic extends Model
         'excerpt',
         'slug'
     ];
+
+    /* Eloquent Relationships */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
